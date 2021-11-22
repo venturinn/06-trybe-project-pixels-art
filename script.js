772dd6adabc2799bd98d0fd9a350813081eef1c2
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-22 16:16:57 -0300

    Adicionados os requisitos 04 e 05 do projeto

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ for (let i = 0; i < palheta.length; i += 1) {
     palheta[i].style.backgroundColor = cores[i]
 
 }
+
+// Adicionando 25 pixels ao elemento id pixel-board
+
+let pixelBoard = document.getElementById('pixel-board');
+
+for (let i = 0; i < 25; i += 1) {
+
+    let pixel = document.createElement('div');
+
+    pixel.className = 'pixel';
+    pixel.style.height = '40px';
+    pixel.style.width = '40px';
+    pixel.style.border = '1px solid black';
+    pixel.style.display = 'inline-block';
+    pixel.style.backgroundColor = 'white';
+
+    pixelBoard.appendChild(pixel);
+}
