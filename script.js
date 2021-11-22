e41a467883bce29f9c42e1a04ff1b9a4f43b9525
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-22 16:48:50 -0300

    Adicionado o requisito 07 do projeto

--- script.js
+++ script.js
@@ -32,3 +32,39 @@ for (let i = 0; i < 25; i += 1) {
 // Definindo a cor preta como sendo a cor inicial
 
 palheta[0].className = 'color select';
+
+// Aplicando a seleção das cores da palheta conforme o click
+// Lembrando: a variável palheta possui o array com as 4 opções de cor
+
+
+function selectReset() {
+    for (let i = 0; i < palheta.length; i += 1) {
+        palheta[i].className = 'color';
+    }
+}
+
+function select01(event) {
+    selectReset()
+    palheta[0].className = 'color select';
+}
+
+function select02(event) {
+    selectReset()
+    palheta[1].className = 'color select';
+}
+
+function select03(event) {
+    selectReset()
+    palheta[2].className = 'color select';
+}
+
+function select04(event) {
+    selectReset()
+    palheta[3].className = 'color select';
+}
+
+
+palheta[0].addEventListener('click', select01);
+palheta[1].addEventListener('click', select02);
+palheta[2].addEventListener('click', select03);
+palheta[3].addEventListener('click', select04);
