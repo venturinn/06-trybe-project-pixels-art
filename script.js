22a987f979f7bb15b647b97c49e4906b946e4502
¨Venturin¨ <diegoventurinn@gmail.com>
2021-11-23 08:15:07 -0300

    Acertando a indentação do código

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
 // Aplicando cores para a palheta de corres, sendo a primeira black e as outras aleatórias
 
+const palheta = document.getElementsByClassName('color');
 
-let palheta = document.getElementsByClassName('color');
-
-
-let cores = ['black']
-
+const cores = ['black'];
 
 for (let i = 0; i < palheta.length; i += 1) {
+  const r = String(Math.random() * 255);
+  const g = String(Math.random() * 255);
+  const b = String(Math.random() * 255);
 
-    cores.push('rgb' + '(' + String(Math.random() * 255) + ',' + String(Math.random() * 255) + ',' + String(Math.random() * 255) + ')');
-    palheta[i].style.backgroundColor = cores[i]
-    console.log(cores);
-
+  cores.push(`${'rgb' + '('}${r},${g},${b})`);
+  palheta[i].style.backgroundColor = cores[i];
+  console.log(cores);
 }
-
 
 // Adicionando 25 pixels ao elemento id pixel-board
 
-let pixelBoard = document.getElementById('pixel-board');
+const pixelBoard = document.getElementById('pixel-board');
 
 let pixelNumer = 25;
 
-creatPixels();
-
 function creatPixels() {
+  for (let i = 0; i < pixelNumer; i += 1) {
+    const pixel = document.createElement('div');
 
-    for (let i = 0; i < pixelNumer; i += 1) {
+    pixel.className = 'pixel';
+    pixel.style.height = '40px';
+    pixel.style.width = '40px';
+    pixel.style.border = '1px solid black';
+    pixel.style.display = 'inline-block';
+    pixel.style.backgroundColor = 'white';
 
-        let pixel = document.createElement('div');
-
-        pixel.className = 'pixel';
-        pixel.style.height = '40px';
-        pixel.style.width = '40px';
-        pixel.style.border = '1px solid black';
-        pixel.style.display = 'inline-block';
-        pixel.style.backgroundColor = 'white';
-
-        pixelBoard.appendChild(pixel);
-    }
+    pixelBoard.appendChild(pixel);
+  }
 }
+
+creatPixels();
 
 // Definindo a cor preta como sendo a cor inicial
 
@@ -48,31 +44,30 @@ palheta[0].className = 'selected color';
 // Aplicando a seleção das cores da palheta conforme o click
 // Lembrando: a variável palheta possui o array com as 4 opções de cor
 
-
 function selectReset() {
-    for (let i = 0; i < palheta.length; i += 1) {
-        palheta[i].className = 'color';
-    }
+  for (let i = 0; i < palheta.length; i += 1) {
+    palheta[i].className = 'color';
+  }
 }
 
-function select01(event) {
-    selectReset()
-    palheta[0].className = 'color selected';
+function select01() {
+  selectReset();
+  palheta[0].className = 'color selected';
 }
 
-function select02(event) {
-    selectReset()
-    palheta[1].className = 'color selected';
+function select02() {
+  selectReset();
+  palheta[1].className = 'color selected';
 }
 
-function select03(event) {
-    selectReset()
-    palheta[2].className = 'color selected';
+function select03() {
+  selectReset();
+  palheta[2].className = 'color selected';
 }
 
-function select04(event) {
-    selectReset()
-    palheta[3].className = 'color selected';
+function select04() {
+  selectReset();
+  palheta[3].className = 'color selected';
 }
 
 palheta[0].addEventListener('click', select01);
@@ -80,91 +75,76 @@ palheta[1].addEventListener('click', select02);
 palheta[2].addEventListener('click', select03);
 palheta[3].addEventListener('click', select04);
 
-
 // Pintando os pixels com a cor selecionada
 
 function paint(event) {
+  const selectColor = document.querySelector('.selected');
+  const color = selectColor.style.backgroundColor;
 
-    let selectColor = document.querySelector('.selected');
-    let color = selectColor.style.backgroundColor;
-
-    if (event.target.className === 'pixel') {
-        event.target.style.backgroundColor = color;
-    }
+  if (event.target.className === 'pixel') {
+    event.target.style.backgroundColor = color;
+  }
 }
 
 document.addEventListener('click', paint);
 
 // Limpando o quadro de Pixels:
 
-
 function clear() {
+  const allPixels = document.getElementsByClassName('pixel');
 
-    let allPixels = document.getElementsByClassName('pixel');
-
-    for (let i = 0; i < allPixels.length; i += 1) {
-
-        allPixels[i].style.backgroundColor = 'white';
-    }
+  for (let i = 0; i < allPixels.length; i += 1) {
+    allPixels[i].style.backgroundColor = 'white';
+  }
 }
 
-let button = document.querySelector('#clear-board');
+const button = document.querySelector('#clear-board');
 button.addEventListener('click', clear);
 
-// Gerando um quadro com tamanho escolhido pelo usuário 
-
+// Gerando um quadro com tamanho escolhido pelo usuário
 
 // Função para zerar os pixels
 
-let pixelBoard2 = document.getElementById('pixel-board')
-let pixels = document.getElementsByClassName('pixel')
+const pixelBoard2 = document.getElementById('pixel-board');
+const pixels = document.getElementsByClassName('pixel');
 
 function resetBoard() {
-
-    for (; pixels.length > 0;) {
-        pixelBoard2.removeChild(pixels[0]);
-    }
+  for (; pixels.length > 0;) {
+    pixelBoard2.removeChild(pixels[0]);
+  }
 }
 
 function newBoard(event) {
+  if (event.key === 'Enter' || event.type === 'click') {
+    if (enter.value === '') {
+      alert('Board inválido!');
+    } else if (enter.value <= 5) {
+      pixelNumer = 25;
+      pixelBoard2.style.height = '210px';
+      pixelBoard2.style.width = '210px';
+      resetBoard();
+      creatPixels();
+    } else if (enter.value > 50) {
+      pixelNumer = 2500;
+      pixelBoard2.style.height = '2100px';
+      pixelBoard2.style.width = '2100px';
+      resetBoard();
+      creatPixels();
+    } else {
+      pixelNumer = enter.value * enter.value;
+      const lado = ((enter.value * 40) + (enter.value * 2));
+      const ladoString = String(lado);
 
+      pixelBoard2.style.height = `${ladoString}px`;
+      pixelBoard2.style.width = `${ladoString}px`;
 
-    if (event.key === 'Enter' || event.type === 'click') {
-
-        if (enter.value === '') {
-            alert('Board inválido!');
-        } else if (enter.value <= 5) {
-
-            pixelNumer = 25;
-            pixelBoard2.style.height = '210px';
-            pixelBoard2.style.width = '210px';
-            resetBoard();
-            creatPixels();
-
-        } else if (enter.value > 50) {
-
-            pixelNumer = 2500;
-            pixelBoard2.style.height = '2100px';
-            pixelBoard2.style.width = '2100px';
-            resetBoard();
-            creatPixels();
-
-        } else {
-
-            pixelNumer = enter.value * enter.value;
-            let lado = ((enter.value * 40) + (enter.value * 2));
-            let ladoString = String(lado);
-
-            pixelBoard2.style.height = ladoString + 'px';
-            pixelBoard2.style.width = ladoString + 'px';
-
-            resetBoard();
-            creatPixels();
-        }
+      resetBoard();
+      creatPixels();
     }
+  }
 }
 
-let newClick = document.getElementById('generate-board');
+const newClick = document.getElementById('generate-board');
 newClick.addEventListener('click', newBoard);
 
 let enter = document.getElementById('board-size');
